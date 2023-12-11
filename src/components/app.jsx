71afc048813c4b26d14cff3ee2997a56fafd4871
ahmadnusa <ahmadnursaefullah79@gmail.com>
2023-12-11 20:53:47 +0700

    add new components

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header'; // Fix the casing of the file name
+import Footer from './footer'; // Fix the casing of the file name
+import Note from './note'; // Fix the casing of the file name
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
