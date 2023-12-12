212a5aab7630d70be5a835a4629b16fd54b2b5ca
ahmadnusa <ahmadnursaefullah79@gmail.com>
2023-12-12 16:11:22 +0700

    add notes.js

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Header from './header'; // Fix the casing of the file name
-import Footer from './footer'; // Fix the casing of the file name
-import Note from './note'; // Fix the casing of the file name
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+import notes from "../notes";
+
+const note = notes.map((noteItem) => (
+  <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />
+));
 
 function App() {
-    return (
-        <div>
-            <Header />
-            <Note />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      {note}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
-
-    return (
-        <footer>
-            <p>&copy; {currentYear} Quantum Solutions. All rights reserved.</p>
-        </footer>
-    );
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>&copy; {currentYear} Quantum Solutions. All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
